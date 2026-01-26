e1d31a7f5da54ec41acac4033d9b6e7e94baaa0d
Dennis <hej@dennishatwieger.de>
2026-01-26 22:35:38 +0100

    Fix Firebase version consistency - use 10.7.0 across all modules

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCyGi3CcUWBPSyaSbAyCaTyNZNzpLWUqrU",
